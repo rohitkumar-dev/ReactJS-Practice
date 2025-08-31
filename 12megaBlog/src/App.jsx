b6388e442fe6f1decbe265f92780935d62690874
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-08-31 22:43:48 +0530

    mega blog project

---
+++ 12megaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    
+    <>
+      <h2 className='bg-gray-700 mt-20'>Hello</h2>
+    </>
+  )
+}
+
+export default App
