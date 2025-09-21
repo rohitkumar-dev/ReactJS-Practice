af17cae6575424caeef8577846662616ccf6cefe
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-09-21 21:56:53 +0530

    updated

--- 13tips/src/App.jsx
+++ 13tips/src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggesIn] = useState(null)
 
   return (
-    <>
+    <div style={{padding: "2px"}}>
       <h2>React Production Mistake</h2>
-    </>
+      <button onClick={()=> setIsLoggesIn(!isLoggedIn)}>Toggle Login</button>
+
+      <div>
+        <h2>&& operator</h2>
+        {isLoggedIn && <p>Welcome to dashboard</p>}
+      </div>
+      <br />
+      <div>
+        <h2>Ternary operator</h2>
+        {isLoggedIn ? <p>Welcome to dashboard</p> : <p>Please login</p>}
+      </div>
+    </div>
   )
 }
 
