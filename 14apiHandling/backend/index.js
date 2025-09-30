fe38ac436a4b3b679bdbb5818454fa5fde24bf59
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-09-30 21:54:33 +0530

    updared

---
+++ 14apiHandling/backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+
+
+app.get("/", (req,res)=>{
+    res.json({"data": "Server is up"})
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening at http://localhost:${PORT}`)
+})
+
